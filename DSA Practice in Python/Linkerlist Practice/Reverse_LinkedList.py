027a947965a528dd9f0afd3e4f6ff2e6795d0ef6
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2024-10-28 20:37:37 +0530

    Update Reverse_LinkedList.py

--- DSA Practice in Python/Linkerlist Practice/Reverse_LinkedList.py
+++ DSA Practice in Python/Linkerlist Practice/Reverse_LinkedList.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous = None
+        current = head
+        while current:
+            next = current.next
+            current.next = previous
+            previous = current
+            current = next
+        return previous
