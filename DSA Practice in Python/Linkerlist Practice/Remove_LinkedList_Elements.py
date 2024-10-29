09ef02014a786779862082a6df26ed7903d5a787
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2024-10-29 15:09:51 +0530

    Update Remove_LinkedList_Elements.py

--- DSA Practice in Python/Linkerlist Practice/Remove_LinkedList_Elements.py
+++ DSA Practice in Python/Linkerlist Practice/Remove_LinkedList_Elements.py
@@ -1,1 +1,19 @@
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        temp = head
 
+        while temp and temp.val == val:
+            temp = temp.next
+        
+        ret = temp
+        prev = temp
+
+        while temp:
+            if temp.val != val:
+                if temp != prev:
+                    prev.next = temp
+                prev = temp
+            temp = temp.next
+        if prev:
+            prev.next = None
+        return ret
